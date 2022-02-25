7541e92ff6d017e25d271d29dd7464e48409ad3c
Tim Payne <tim.tpayne@gmail.com>
2022-02-25 22:27:29 +0000

    Code tidy up

--- nodejs/webapp/simplesvr.js
+++ nodejs/webapp/simplesvr.js
@@ -1,11 +1,12 @@
-var http = require('http'); // Import Node.js core module
+// Import required core modules
+var http = require('http');
 var https = require('https');
 
-var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/') { //check the URL of the current request
-        // set response header
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        // set response content    
+// Launch the server
+var server = http.createServer(function (req, res) {
+    // Match the paths...
+    if (req.url == '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is the home Page.</p></body></html>');
         res.end();
     }
@@ -20,43 +21,46 @@ var server = http.createServer(function (req, res) {   //create web server
         res.end();
     }
     else if (req.url == "/repostring") {
-    	const options = {
-		  headers: { 'User-Agent': 'Mozilla/5.0' }
-		};
-		https.get("https://api.github.com/users/tpayne/repos", options, function(resp) {
-    		let data = '';
-			resp.on('data', (chunk) => {
-				data += chunk;
-			});
-			resp.on('end', () => {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write('<html><body><p>');
-                //res.write(JSON.stringify(data, null, 4));
-                res.write(data);
-                res.write('</p></body></html>');
-                res.end();                
-			});
-		}).on("error", (err) => {
-			console.log("Error: ", err.message);
-		});
-    }    
+        const options = {
+            headers: { 'User-Agent': 'Mozilla/5.0' }
+        };
+        https.get("https://api.github.com/users/tpayne/repos",
+            options, function (resp) {
+                let data = '';
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write('<html><body><p>');
+                    //res.write(JSON.stringify(data, null, 4));
+                    res.write(data);
+                    res.write('</p></body></html>');
+                    res.end();
+                });
+            }).on("error", (err) => {
+                console.log("Error: ", err.message);
+            });
+    }
     else
         res.end('Invalid Request!');
+}).on("error", (err) => {
+    console.log("Error: ", err.message);
 });
 
 // Signal handlers...
 process.on('SIGTERM', () => {
- server.close(() => {
-     console.log('Kill server');
- })
+    server.close(() => {
+        console.log('Kill server');
+    })
 })
 
 process.on('SIGINT', () => {
- server.close(() => {
-    console.log('Kill server');
-})
+    server.close(() => {
+        console.log('Kill server');
+    })
 })
 
-server.listen(8080); 
+server.listen(8080);
 
 console.log('Node.js web server at port 8080 is running...')
