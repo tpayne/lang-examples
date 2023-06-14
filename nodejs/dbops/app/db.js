b95f56bb838a905972f758ef64d2e16210751c0a
Tim Payne <tim.tpayne@gmail.com>
2023-06-14 09:14:32 +0100

    This app needs to be Dockerised testing

---
+++ nodejs/dbops/app/db.js
@@ -0,0 +1,67 @@
+const PropertiesReader = require('properties-reader')
+const Pool = require('pg').Pool
+
+// Database connections
+var properties = PropertiesReader('config/app.properties')
+const pgConStr = properties.get("pg_constr")
+const dbconfig = parser(pgConStr)
+
+const pool = new Pool(
+    dbconfig
+)
+
+// Short parser function to workaround weirdo issues from Pool...
+function parser(conStr) {
+    var tmpStr = conStr.substring(conStr.indexOf('://')+3)
+    var userId = tmpStr.substring(0,tmpStr.indexOf(':'))
+    var tmpStr = tmpStr.substring(tmpStr.indexOf(':')+1)
+    var pwd = tmpStr.substring(0,tmpStr.indexOf('@'))
+    var tmpStr = tmpStr.substring(tmpStr.indexOf('@')+1)
+    var host = tmpStr.substring(0,tmpStr.indexOf('/'))
+    var db = tmpStr.substring(tmpStr.indexOf('/')+1)
+    return { host: host, user: userId, password: pwd, database: db }
+}
+
+const getOrdersInProgress = (request, response) => {
+    pool.query('SELECT customer_name,order_name,stock_item,order_date,number_ordered FROM orders_in_progress', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getCustomers = (request, response) => {
+    pool.query('SELECT * FROM customers ORDER BY customer_uid ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getStock = (request, response) => {
+    pool.query('SELECT * FROM stock_items ORDER BY stock_uid ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    getOrdersInProgress,
+    getCustomers,
+    getStock,
+}
+
+// Signal handlers...
+function signalHandler(signal) {
+    pool.end()
+    console.log("Killing process and shutting down")
+    process.exit()
+}
+
+process.on('SIGINT', signalHandler)
+process.on('SIGTERM', signalHandler)
+process.on('SIGQUIT', signalHandler)
---
+++ nodejs/dbops/app/restapi.js
@@ -0,0 +1,45 @@
+// Import required core modules
+const express = require('express')
+const fs = require('fs');
+const db = require('./db')
+
+const bodyParser = require('body-parser')
+
+const PropertiesReader = require('properties-reader')
+var properties = PropertiesReader('config/app.properties')
+const port = properties.get("port")
+
+const svrapp = express()
+
+svrapp.use(bodyParser.json())
+svrapp.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+svrapp.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
+// Standard functions...
+svrapp.get('/', (request, response) => {
+    fs.readFile('html/index.html', function (herr, html) {
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.write(html)
+        response.end()
+    })
+})
+
+svrapp.get('/info', (request, response) => {
+    response.json({ info: 'This is a Demoapp for Postgres' })
+})
+
+svrapp.get('/version', (request, response) => {
+    response.json({ version: '1.0.0' })
+})
+
+// Database functions
+svrapp.get('/orders', db.getOrdersInProgress)
+svrapp.get('/customers', db.getCustomers)
+svrapp.get('/stock', db.getStock)
