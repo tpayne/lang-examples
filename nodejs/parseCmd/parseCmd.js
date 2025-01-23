5b78d52fbb045aa9c6d24b61d22db23fce326602
tpayne <tpayne@noreply.github.com>
2025-01-23 22:41:26 +0000

    Update

--- nodejs/parseCmd/parseCmd.js
+++ nodejs/parseCmd/parseCmd.js
@@ -1,13 +1,17 @@
 // Imports...
 const args = require('yargs').argv
-const isHelmAvailable = require('hasbin').sync('helm')
-
+const exeTest = require('hasbin')
+const { boolean } = require('yargs')
+ 
 // Do function...
 function main(args) {
     try {
         console.log(`Generate file: ${args.generateFile}`)
         console.log(`Overwrite: ${args.overwrite}`)
+        isHelmAvailable = exeTest.sync('helm')
+        isLsAvailable = exeTest.sync('ls')
         console.log(`Is Helm installed: ${isHelmAvailable}`)
+        console.log(`Is LS installed: ${isLsAvailable}`)
     } catch (e) {
         console.log(e)
     }
