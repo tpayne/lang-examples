0de6ae9cdddb784757363b2ac8651c164fcf6eb4
tpayne <tpayne@noreply.github.com>
2025-01-23 23:49:58 +0000

    Update

--- nodejs/parseCmd/parseCmd.js
+++ nodejs/parseCmd/parseCmd.js
@@ -2,7 +2,20 @@
 const args = require('yargs').argv
 const exeTest = require('hasbin')
 const { boolean } = require('yargs')
- 
+
+const fs = require('fs')
+const chdPrc = require('child_process')
+
+function logOutput(fileName) {
+    try {
+        const lsProc = chdPrc.spawnSync('ls', ['-lh', '/bin']);
+        fs.writeFileSync(fileName, lsProc.stdout)
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+
 // Do function...
 function main(args) {
     try {
@@ -12,6 +25,15 @@ function main(args) {
         isLsAvailable = exeTest.sync('ls')
         console.log(`Is Helm installed: ${isHelmAvailable}`)
         console.log(`Is LS installed: ${isLsAvailable}`)
+        logOutput(args.generateFile)
+        fs.stat(args.generateFile, function(err, stat) {
+            if (err == null) {
+                buf = fs.readFileSync(args.generateFile).toString()
+                console.log('Command output:\n'+buf)
+            } else if (err.code === 'ENOENT') {
+                console.log(`File ${args.generateFile} does not exist`)
+            }
+        });
     } catch (e) {
         console.log(e)
     }
