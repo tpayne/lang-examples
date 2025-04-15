cc5b7230968119b91dc81c90ef754382996ac6f4
tpayne <tpayne@noreply.github.com>
2025-04-15 10:22:25 +0100

    feat: Simplify logic

--- nodejs/genai/openai/chatbot/src/codeReviews.js
+++ nodejs/genai/openai/chatbot/src/codeReviews.js
@@ -1,7 +1,4 @@
 const logger = require('./logger');
-const OpenAI = require('openai');
-
-const { getConfig } = require('./properties');
 
 const {
   fetchRepoContentsRecursive,
@@ -13,40 +10,9 @@ const {
   readFilesInDirectory,
 } = require('./utilities');
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 /* eslint-disable no-restricted-syntax, no-await-in-loop, consistent-return */
 
-async function analyzeCode(fileContent,
-  analysisRequest, ai) {
-  try {    
-    const response = await openai.chat.completions.create({
-      model: getConfig().aiModel,
-      messages: [
-        {
-          role: 'system',
-          content: 'You are a helpful assistant that analyzes code '+
-                   ' and spots issues, security problems and bugs. '+
-                   'You explain the issues, but aim to be concise',
-        },
-        {
-          role: 'user',
-          content: `${analysisRequest}:\n\n\`\`\`\n${fileContent}\n\`\`\``,
-        },
-      ],
-      max_tokens: Number(getConfig().maxTokens),
-      temperature: 0.2, // Lower temperature for more focused analysis
-    });
-
-    if (response.choices && response.choices[0].message &&
-      response.choices[0].message.content) {
-      return response.choices[0].message.content.trim();
-    }
-    return null;
-  } catch(error) {
-    throw error;
-  }
-}
 /**
  * Lists the names of public repositories for a given GitHub username.
  * Fetches repo data and extracts the 'name' property.
@@ -55,12 +21,10 @@ async function analyzeCode(fileContent,
  * @param {string} username The GitHub username.
  * @param {string} repoName The GitHub repo name.
  * @param {string} repoPath The GitHub path name.
- * @returns {Promise<string[]>} Array of public repository names.
+ * @returns {Promise<string[]>} Array of public file content.
  * @throws {Error} If API request fails or user is not found.
  */
 async function codeReviews(username, repoName, repoPath) {
-  const analysisRequest = 'Explain what this code does and identify' +
-                          ' any potential issues or areas for improvement.';
   const responseMap = new Map();
   try {
     const tmpDir = await createUniqueTempDir();
@@ -86,10 +50,8 @@ async function codeReviews(username, repoName, repoPath) {
       if (!fileContent) {
         continue;
       }
-
-      const analysis = await analyzeCode(fileContent, analysisRequest, openai);
+      responseMap.set(fileN,fileContent);
       count++;
-      responseMap.set(fileN, analysis); 
     }
 
     return Array.from(responseMap.entries()).map(([fileN, analysis]) => ({
