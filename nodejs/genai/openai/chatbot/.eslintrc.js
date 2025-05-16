6fd958bfe91eacedf3797f8b68eb7d801488a6b9
Tim Payne <tpayne@users.noreply.github.com>
2025-05-16 02:16:25 +0100

    WIP

--- nodejs/genai/openai/chatbot/.eslintrc.js
+++ nodejs/genai/openai/chatbot/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   rules: {
     'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
     'max-len': ['error', {
-      code: 150, // Set the desired maximum line length here (e.g., 120)
+      code: 200, // Set the desired maximum line length here (e.g., 120)
       ignoreUrls: true, // Optional: Ignore lines containing URLs
       ignoreStrings: false, // Optional: Apply limit to string literals (default is often false)
       ignoreTemplateLiterals: false, // Optional: Apply limit to template literals (default is often false)
